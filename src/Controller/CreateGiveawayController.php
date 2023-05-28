5cdf8524ab5b361b17ae110e6d1c44d8c7899901
yosraLR <yosra.larhdid12@gmail.com>
2023-05-29 01:17:35 +0200

    create prizes(lots) for giveaways

--- src/Controller/CreateGiveawayController.php
+++ src/Controller/CreateGiveawayController.php
@@ -9,11 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class CreateGiveawayController extends AbstractController
 {
     #[Route('create_giveaway', name: 'create_giveaway')]
-    public function createGiveaway(Request $request, EntityManagerInterface $entityManager): Response
+    public function createGiveaway(Request $request, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
         
         $giveaway = new Giveaways();
@@ -25,11 +27,14 @@ class CreateGiveawayController extends AbstractController
             $entityManager->persist($giveaway);
             $entityManager->flush();
 
-            return $this->redirectToRoute('create_giveaway');
+            // $this->addFlash('success', 'Giveaway created successfully!');
+
+            return $this->redirectToRoute('prize', ['giveawayId' => $giveaway->getId()]);
         }
 
         return $this->render('main/create_giveaway.html.twig', [
             'giveawayForm' => $form->createView(),
+            'giveawayId' => $giveaway->getId(),
         ]);
     }
 }
