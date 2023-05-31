bc0eb4b88ed2f222c21c3dc8692c9412e09644b7
manelbelhocine <manelmanel92@yahoo.com>
2023-05-31 23:51:14 +0200

    synchronisation BDD and view

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,19 +1,35 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\GiveawaysRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// class MainController extends AbstractController
+// {
+//     #[Route('app_main', name: 'app_main')]
+//     public function index(): Response
+//     {
+//         return $this->render('main/index.html.twig', [
+//             'controller_name' => 'MainController',
+//         ]);
+//     }
+
+// }
+
+
+
 class MainController extends AbstractController
 {
     #[Route('app_main', name: 'app_main')]
-    public function index(): Response
+    public function index(GiveawaysRepository $giveawaysRepository): Response
     {
+        $giveaways = $giveawaysRepository->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'giveaways' => $giveaways,
         ]);
     }
-
 }
