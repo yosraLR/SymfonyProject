8a7cc383f786ebb02275ded1577fa35d79bb28bb
manelbelhocine <manelmanel92@yahoo.com>
2023-06-04 12:34:36 +0200

    Logged In page and redirection

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,22 +1,46 @@
 <?php
 
 namespace App\Controller;
-use App\Repository\GiveawaysRepository;
 
+use App\Repository\GiveawaysRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class MainController extends AbstractController
 {
-    #[Route('app_main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(GiveawaysRepository $giveawaysRepository): Response
     {
         $giveaways = $giveawaysRepository->findAll();
 
-        return $this->render('main/index.html.twig', [
+        if ($this->getUser()) {
+            // If the user is logged in, redirect to the logged-in page
+            return $this->redirectToRoute('app_logged_in');
+        }
+
+        else return $this->render('main/index.html.twig', [
             'giveaways' => $giveaways,
         ]);
     }
+    
+    #[Route('/logged-in', name: 'app_logged_in')]
+    public function loggedIn(GiveawaysRepository $giveawaysRepository): Response
+    {
+        $giveaways = $giveawaysRepository->findAll();
+    
+        $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
+
+        if ($this->getUser()) {
+            // If the user is logged in, render the logged-in page
+            return $this->render('main/logged_in_page.html.twig',[
+            'giveaways' => $giveaways,
+            'userName' => $userName,
+        ]);
+        } else {
+            // User is not logged in, redirect to the main page
+            return $this->redirectToRoute('app_main');
+        }
+    }
+    
 }
