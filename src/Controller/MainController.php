ccc6cb16b5d780189cefd9d6ca31f9d5441d044a
manelbelhocine <manelmanel92@yahoo.com>
2023-06-04 13:31:59 +0200

    Date constraint for giveaways

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,14 +12,19 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(GiveawaysRepository $giveawaysRepository): Response
     {
-        $giveaways = $giveawaysRepository->findAll();
+        $currentDate = new \DateTime();
+        $giveaways = $giveawaysRepository->createQueryBuilder('date')
+            ->andWhere('date.EndDate >= :currentDate')
+            ->setParameter('currentDate', $currentDate)
+            ->getQuery()
+            ->getResult();
 
         if ($this->getUser()) {
             // If the user is logged in, redirect to the logged-in page
             return $this->redirectToRoute('app_logged_in');
         }
 
-        else return $this->render('main/index.html.twig', [
+        return $this->render('main/index.html.twig', [
             'giveaways' => $giveaways,
         ]);
     }
@@ -27,20 +32,23 @@ class MainController extends AbstractController
     #[Route('/logged-in', name: 'app_logged_in')]
     public function loggedIn(GiveawaysRepository $giveawaysRepository): Response
     {
-        $giveaways = $giveawaysRepository->findAll();
-    
+        $currentDate = new \DateTime();
+        $giveaways = $giveawaysRepository->createQueryBuilder('date')
+            ->andWhere('date.EndDate > :currentDate')
+            ->setParameter('currentDate', $currentDate)
+            ->getQuery()
+            ->getResult();
         $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
 
         if ($this->getUser()) {
             // If the user is logged in, render the logged-in page
-            return $this->render('main/logged_in_page.html.twig',[
-            'giveaways' => $giveaways,
-            'userName' => $userName,
-        ]);
+            return $this->render('main/logged_in_page.html.twig', [
+                'giveaways' => $giveaways,
+                'userName' => $userName,
+            ]);
         } else {
             // User is not logged in, redirect to the main page
             return $this->redirectToRoute('app_main');
         }
     }
-    
 }
