90540b110fb2154f302a2c80b6d6c505b545f8a9
manelbelhocine <manelmanel92@yahoo.com>
2023-06-05 14:15:52 +0200

    Gestion de roles suite

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,9 +6,18 @@ use App\Repository\GiveawaysRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
 
 class MainController extends AbstractController
 {
+    private $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
     #[Route('/', name: 'app_main')]
     public function index(GiveawaysRepository $giveawaysRepository): Response
     {
@@ -23,6 +32,7 @@ class MainController extends AbstractController
             // If the user is logged in, redirect to the logged-in page
             return $this->redirectToRoute('app_logged_in');
         }
+        
 
         return $this->render('main/index.html.twig', [
             'giveaways' => $giveaways,
@@ -40,18 +50,19 @@ class MainController extends AbstractController
             ->getResult();
         $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
 
-        if ($this->getUser()) {
+        if ($this->authorizationChecker->isGranted('ROLE_USER')) {
             // If the user is logged in, render the logged-in page
             return $this->render('user/index.html.twig', [
                 'giveaways' => $giveaways,
                 'userName' => $userName,
             ]);
-        } else {
+        } 
+        else {
             // User is not logged in, redirect to the main page
             return $this->redirectToRoute('app_main');
         }
     }
-    #[Route('user/organisator', name: 'organisator')]
+    #[Route('/organisator', name: 'organisator')]
     public function Organisator(GiveawaysRepository $giveawaysRepository): Response
     {
         $currentDate = new \DateTime();
@@ -61,14 +72,14 @@ class MainController extends AbstractController
             ->getQuery()
             ->getResult();
         $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
-
-        if ($this->getUser()) {
+        
+        if ($this->authorizationChecker->isGranted('ROLE_ORGANISATOR')) {
             // If the user is logged in, render the logged-in page
             return $this->render('organisator/index.html.twig', [
                 'giveaways' => $giveaways,
                 'userName' => $userName,
             ]);
-        } else {
+        }  else {
             // User is not logged in, redirect to the main page
             return $this->redirectToRoute('app_main');
         }
