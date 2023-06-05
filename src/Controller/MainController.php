5e8032bb48655d8dd471b22bb2cb374d67d11786
manelbelhocine <manelmanel92@yahoo.com>
2023-06-05 22:30:52 +0200

    Redirection de routes avec roles + frontpages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,100 +18,100 @@ class MainController extends AbstractController
         $this->authorizationChecker = $authorizationChecker;
     }
 
-    #[Route('/', name: 'app_main')]
-    public function index(GiveawaysRepository $giveawaysRepository): Response
+        #[Route('/', name: 'app_main')]
+        public function index(): Response
     {
-        $currentDate = new \DateTime();
-        $giveaways = $giveawaysRepository->createQueryBuilder('date')
-            ->andWhere('date.EndDate >= :currentDate')
-            ->setParameter('currentDate', $currentDate)
-            ->getQuery()
-            ->getResult();
-
-        if ($this->getUser()) {
-            // If the user is logged in, redirect to the logged-in page
-            return $this->redirectToRoute('app_logged_in');
-        }
-        
-
-        return $this->render('main/index.html.twig', [
-            'giveaways' => $giveaways,
-        ]);
-    }
-    
-    #[Route('/user', name: 'app_logged_in')]
-    public function User(GiveawaysRepository $giveawaysRepository): Response
-    {
-        $currentDate = new \DateTime();
-        $giveaways = $giveawaysRepository->createQueryBuilder('date')
-            ->andWhere('date.EndDate > :currentDate')
-            ->setParameter('currentDate', $currentDate)
-            ->getQuery()
-            ->getResult();
-        $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
-
-        if ($this->authorizationChecker->isGranted('ROLE_USER')) {
-            // If the user is logged in, render the logged-in page
-            return $this->render('user/index.html.twig', [
-                'giveaways' => $giveaways,
-                'userName' => $userName,
-            ]);
-        } 
-        else {
-            // User is not logged in, redirect to the main page
-            return $this->redirectToRoute('app_main');
+        if ($this->isGranted('ROLE_USER') && !($this->isGranted('ROLE_ORGANISATOR'))) {
+            return $this->redirectToRoute('userpage');
+        } elseif ($this->isGranted('ROLE_ORGANISATOR')) {
+            return $this->redirectToRoute('organisator');
+        } else {
+            return $this->redirectToRoute('homepage');
         }
     }
-    // #[Route('/organisator', name: 'organisator')]
-    // public function Organisator(GiveawaysRepository $giveawaysRepository): Response
-    // {
-    //     $currentDate = new \DateTime();
-    //     $giveaways = $giveawaysRepository->createQueryBuilder('date')
-    //         ->andWhere('date.EndDate > :currentDate')
-    //         ->setParameter('currentDate', $currentDate)
-    //         ->getQuery()
-    //         ->getResult();
-    //     $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
-        
-    //     if ($this->authorizationChecker->isGranted('ROLE_ORGANISATOR')) {
-    //         // If the user is logged in, render the logged-in page
-    //         return $this->render('organisator/index.html.twig', [
-    //             'giveaways' => $giveaways,
-    //             'userName' => $userName,
-    //         ]);
-    //     }  else {
-    //         // User is not logged in, redirect to the main page
-    //         return $this->redirectToRoute('app_main');
-    //     }
-    // }
-
-
-
-    #[Route('/organisator', name: 'organisator')]
-    public function Organisator(GiveawaysRepository $giveawaysRepository): Response
-    {
-        $currentDate = new \DateTime();
-        $currentUser = $this->getUser();
-        $giveaways = [];
-    
-        if ($currentUser && $this->authorizationChecker->isGranted('ROLE_ORGANISATOR')) {
-            $giveaways = $giveawaysRepository->createQueryBuilder('g')
-                ->join('g.OrganisatorID', 'o')
-                ->andWhere('o.id = :userId')
-                ->andWhere('g.EndDate > :currentDate')
-                ->setParameter('userId', $currentUser)
+        #[Route('/home', name: 'homepage')]
+        public function home(GiveawaysRepository $giveawaysRepository): Response
+        {
+            $currentDate = new \DateTime();
+            $giveaways = $giveawaysRepository->createQueryBuilder('date')
+                ->andWhere('date.EndDate >= :currentDate')
                 ->setParameter('currentDate', $currentDate)
                 ->getQuery()
                 ->getResult();
+
+            return $this->render('main/index.html.twig', [
+                'giveaways' => $giveaways,
+            ]);
+         }
+    
+        #[Route('/user', name: 'userpage')]
+        public function User(GiveawaysRepository $giveawaysRepository): Response
+        {
+            $currentDate = new \DateTime();
+            $giveaways = $giveawaysRepository->createQueryBuilder('date')
+                ->andWhere('date.EndDate > :currentDate')
+                ->setParameter('currentDate', $currentDate)
+                ->getQuery()
+                ->getResult();
+            $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
+
+            if ($this->authorizationChecker->isGranted('ROLE_USER')) {
+                return $this->render('user/index.html.twig', [
+                    'giveaways' => $giveaways,
+                    'userName' => $userName,
+                ]);
+            } 
+            else {
+                return $this->redirectToRoute('app_main');
+            }
         }
     
-        $userName = $currentUser ? $currentUser->getEmail() : null;
-    
-        return $this->render('organisator/index.html.twig', [
-            'giveaways' => $giveaways,
-            'userName' => $userName,
-        ]);
+        #[Route('/organisator', name: 'organisator')]
+        public function Organisator(GiveawaysRepository $giveawaysRepository): Response
+        {
+            $currentDate = new \DateTime();
+            $currentUser = $this->getUser();
+            $giveaways = [];
+        
+            if ($currentUser && $this->authorizationChecker->isGranted('ROLE_ORGANISATOR')) {
+                $giveaways = $giveawaysRepository->createQueryBuilder('g')
+                    ->join('g.OrganisatorID', 'o')
+                    ->andWhere('o.id = :userId')
+                    ->andWhere('g.EndDate > :currentDate')
+                    ->setParameter('userId', $currentUser)
+                    ->setParameter('currentDate', $currentDate)
+                    ->getQuery()
+                    ->getResult();
+            }
+        
+            $userName = $currentUser ? $currentUser->getEmail() : null;
+        
+            return $this->render('organisator/index.html.twig', [
+                'giveaways' => $giveaways,
+                'userName' => $userName,
+            ]);
+        }
+        
+        // #[Route('/admin', name: 'admin')]
+        // public function Organisator(GiveawaysRepository $giveawaysRepository): Response
+        // {
+        //     $currentDate = new \DateTime();
+        //     $giveaways = $giveawaysRepository->createQueryBuilder('date')
+        //         ->andWhere('date.EndDate > :currentDate')
+        //         ->setParameter('currentDate', $currentDate)
+        //         ->getQuery()
+        //         ->getResult();
+        //     $userName = $this->getUser() ? $this->getUser()->getEmail() : null;
+            
+        //     if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+        //         // If the user is logged in, render the logged-in page
+        //         return $this->render('organisator/index.html.twig', [
+        //             'giveaways' => $giveaways,
+        //             'userName' => $userName,
+        //         ]);
+        //     }  else {
+        //         // User is not logged in, redirect to the main page
+        //         return $this->redirectToRoute('app_main');
+        //     }
+        // }
     }
-    
-    
-}
