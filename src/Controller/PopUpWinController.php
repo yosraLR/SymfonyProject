f7101ed4f3fce50fa2e6dfb46ce36e37904fe1c8
manelbelhocine <manelmanel92@yahoo.com>
2023-06-07 17:51:31 +0200

    Front + Contrainte d'affichage

--- src/Controller/PopUpWinController.php
+++ src/Controller/PopUpWinController.php
@@ -8,10 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PopUpWinController extends AbstractController
 {
-    #[Route('/result', name: 'app_pop_up_win')]
-    public function index(): Response
+    #[Route('/resultwin', name: 'app_pop_up_win')]
+    public function win(): Response
     {
-        return $this->render('pop_up_win/index.html.twig', [
+        return $this->render('pop_up_win/win.html.twig', [
+            'controller_name' => 'PopUpWinController',
+        ]);
+    }
+    #[Route('/resultlost', name: 'app_pop_up_loser')]
+    public function lose(): Response
+    {
+        return $this->render('pop_up_win/lost.html.twig', [
             'controller_name' => 'PopUpWinController',
         ]);
     }
